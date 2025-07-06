6008f93ac308bbc97019f61f946f697d0a420212
Gavin McCormack <mccormackga@icloud.com>
2025-07-06 16:29:35 -0400

    requirements.txt added. use pip install -r requirements.txt to replicate

--- app/main.py
+++ app/main.py
@@ -3,12 +3,23 @@ from passlib.context import CryptContext
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto");
 
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(post.router)
